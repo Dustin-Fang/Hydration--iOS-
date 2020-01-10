6c9666aae2464d7a93538e830cdebb4e112233b0
16fangd <16fangd@gmail.com>
2020-01-09 21:46:00 -0500

    Initial Commit

---
+++ Hydration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hydration
+//
+//  Created by Dustin Fang on 1/9/20.
+//  Copyright © 2020 Dustin Fang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
